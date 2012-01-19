60b08239d9530ca797480b4c522cbd54e1b19797
BuddyL <buddy@buddylindsey.com>
2012-01-18 18:33:36 -0600

    [initial commit]

---
+++ seenbot.rb
@@ -0,0 +1,16 @@
+require 'cinch'
+Dir["./plugins/*.rb"].each {|file| require file }
+
+$logdir = ENV['IRC_LOGDIR']
+$channels = ["#tulsawebdevs"]
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = 'irc.freenode.net'
+    c.channels = $channels
+    c.nick = "tulsabot"
+    c.plugins.plugins = [Log]
+  end
+end
+
+bot.start
